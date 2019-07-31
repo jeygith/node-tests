c34c8f478115bfb9edfd839388f6e30cc797765b
Jeff Githiri <geoffrey.githiri@andela.com>
2019-07-31 12:39:04 +0300

    organizing tests using describe blocks

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,63 +2,70 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
+describe('Utils', ()=>{
 
-    expect(res).toBe(44);
-    /*if(res !== 44){
-        throw new Error(`Expected 44, but got ${res}.`);
-    }*/
-});
+    describe('#add', ()=>{
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
 
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(3, 4, (sum) => {
-        expect(sum).toBe(7);
-        done();
-    })
-});
+            expect(res).toBe(44);
+            /*if(res !== 44){
+                throw new Error(`Expected 44, but got ${res}.`);
+            }*/
+        });
 
-it('should square a number', () => {
-    var res = utils.square(2);
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(3, 4, (sum) => {
+                expect(sum).toBe(7);
+                done();
+            })
+        });
+    });
 
-    expect(res).toBe(4);
 
-    /*if(res !== 4){
-        throw new Error(`Expected 4, but got ${res}. `);
-    }*/
-});
+    it('should square a number', () => {
+        var res = utils.square(2);
 
-it('should square a number aync', (done) => {
-    utils.asyncSquare(5, (res) => {
-        expect(res).toBe(25);
-        done();
+        expect(res).toBe(4);
+
+        /*if(res !== 4){
+            throw new Error(`Expected 4, but got ${res}. `);
+        }*/
+    });
+
+    it('should square a number aync', (done) => {
+        utils.asyncSquare(5, (res) => {
+            expect(res).toBe(25);
+            done();
+        });
+    });
+
+    it('should expect some values', () => {
+        expect(12).toBe(12);
+        expect({name: 'Jeff'}).toEqual({name: 'Jeff'});
+
+        // toInclude
+        expect([2, 3, 4, 5]);
+
+    });
+
+    it('should verify first and last names are set', () => {
+        var user = {
+            location: 'roysa', age: 26
+        }
+
+        var res = utils.setName(user, 'Jeff Githire');
+
+        console.log(res);
+
+        expect(user).toEqual(res);
+
+        /* expect(res).toInclude({
+             firstName: 'Jeff',
+             lastName: 'Githire'
+         });*/
+
+
     });
 });
 
-it('should expect some values', () => {
-    expect(12).toBe(12);
-    expect({name: 'Jeff'}).toEqual({name: 'Jeff'});
-
-    // toInclude
-    expect([2, 3, 4, 5]);
-
-});
-
-it('should verify first and last names are set', () => {
-    var user = {
-        location: 'roysa', age: 26
-    }
-
-    var res = utils.setName(user, 'Jeff Githire');
-
-    console.log(res);
-
-    expect(user).toEqual(res);
-
-    /* expect(res).toInclude({
-         firstName: 'Jeff',
-         lastName: 'Githire'
-     });*/
-
-
-});
